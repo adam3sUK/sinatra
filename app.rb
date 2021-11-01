e018c3d75dbd80b1d81c36b7d34788ab3d1fdfea
Adam Harbisher <aharbisher@gmail.com>
2021-11-01 15:24:22 +0000

    Installed Sinatra contrib

--- app.rb
+++ app.rb
@@ -1,25 +1,19 @@
 require 'sinatra'
-require "sinatra/reloader"
+require "sinatra/reloader" if development?
 
-class MyApp < Sinatra::Base
-  configure :development do
-    register Sinatra::Reloader
-  end
 
-  get '/' do
-    "hello!"
-  end
-  
-  get '/secret' do
-    "Psssh! I got a secret"
-  end
-  
-  get '/about' do
-    "This is the page for secrets"
-  end
-  
-  get '/contact-us' do
-    "We'll contact you"
-  end
+get '/' do
+  "hello!"
 end
 
+get '/secret' do
+  "Psssh! I got a secret"
+end
+
+get '/about' do
+  "This is the page for secrets"
+end
+
+get '/contact-us' do
+  "We'll contact you"
+end
